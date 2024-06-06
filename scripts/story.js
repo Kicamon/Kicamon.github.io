0c5cf000a3af8caeab9f9c74b7620ffca3acf57e
Kicamon <1532931259@qq.com>
2024-06-07 00:39:59 +0800

    delete excess lines

--- scripts/story.js
+++ scripts/story.js
@@ -1,22 +1,3 @@
-//let $timeline = $(".timeline")
-//
-//let Timeline = function() {
-//  $.getJSON("../static/img-story/timeline.json", function(data) {
-//    $.each(data, function(_, item) {
-//      $timeline.append(`
-//        <div class="item" data-text="《执子之手，与子偕老》">
-//          <div class="content">
-//            <img src="../static/img-story/${item.img}" alt="" class="img">
-//            <h2 class="content-title">${item.time}</h2>
-//            <p class="content-desc">${item.text}</p>
-//          </div>
-//        </div>
-//      `)
-//    })
-//  })
-//}
-//
-//Timeline()
 let $timeline = $(".timeline");
 
 let Timeline = function() {
