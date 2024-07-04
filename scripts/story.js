245f112e8750249105eaa55e3a9f17a0b435309d
Kicamon <1532931259@qq.com>
2024-07-04 21:38:15 +0800

    summer vacation

--- scripts/story.js
+++ scripts/story.js
@@ -3,7 +3,7 @@ let $timeline = $(".timeline");
 let Timeline = function() {
   // 使用同步加载
   let data = $.parseJSON($.ajax({
-    url: "../static/img-story/timeline.json",
+    url: "https://raw.githubusercontent.com/Kicamon/Kicamon.github.io/main/static/img-story/timeline.json",
     dataType: "json",
     async: false
   }).responseText);
@@ -11,7 +11,7 @@ let Timeline = function() {
     $timeline.append(`
       <div class="item" data-text="《执子之手，与子偕老》">
         <div class="content">
-          <img src="../static/img-story/${item.img}" alt="" class="img">
+          <img src="https://raw.githubusercontent.com/Kicamon/Kicamon.github.io/main/static/img-story/${item.img}" alt="" class="img">
           <h2 class="content-title">${item.time}</h2>
           <p class="content-desc">${item.text}</p>
         </div>
